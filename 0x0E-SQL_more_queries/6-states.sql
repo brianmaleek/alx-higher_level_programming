b27fb17e3836bdc165bf4058379fe0fd3ea2c8a2
brianmaleek <maleekmalik@gmail.com>
2023-08-16 16:01:17 +0300

    6-states.sql fix1

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -10,5 +10,5 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- create table if not exists states
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
     id INT NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(256) NOT NULL,
+    name VARCHAR(256) NOT NULL
 );
