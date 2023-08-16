53a99c7bce8c60c33677f29610364c09e3668f18
brianmaleek <maleekmalik@gmail.com>
2023-08-16 16:04:52 +0300

    7-cities.sql fix1

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -10,10 +10,10 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
 -- create table if not exists cities
-CREATE TABLE IF NOT EXISTS cities (
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
     id INT NOT NULL AUTO_INCREMENT UNIQUE,
     state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (state_id) REFERENCES states(id)
+    FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id)
 );
