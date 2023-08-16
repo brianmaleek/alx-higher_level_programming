b16e727002ea97142a384f243aa6d40de08f3a9d
brianmaleek <maleekmalik@gmail.com>
2023-08-16 17:55:43 +0300

    16-shows_by_genre.sql fix1

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -6,8 +6,10 @@
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
 
-SELECT tv_shows.title, IFNULL(tv_genres.name, 'NULL') AS genre
+SELECT tv_shows.title, tv_genres.name
     FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-    LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+LEFT JOIN tv_show_genres
+    ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres
+    ON tv_show_genres.genre_id = tv_genres.id
 ORDER BY tv_shows.title ASC, tv_genres.name ASC;
