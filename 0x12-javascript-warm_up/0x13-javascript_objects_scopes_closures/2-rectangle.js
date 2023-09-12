55c5886481db5ab06346648c20ddf338713ed497
brianmaleek <maleekmalik@gmail.com>
2023-09-12 21:39:26 +0300

    2-rectangle.js

---
+++ 0x12-javascript-warm_up/0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+// class Rectangle that defines a rectangle
+// initializes: w: width, h: height
+// if w or h is equal to 0 or not a positive integer, create an empty object
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      // empty object
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+
+// module.exports exports the class Rectangle
+module.exports = Rectangle;
