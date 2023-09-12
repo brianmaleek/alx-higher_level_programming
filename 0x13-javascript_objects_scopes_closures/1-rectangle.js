4d6f140e34026c0fe683f479f49dacdf0150ce50
brianmaleek <maleekmalik@gmail.com>
2023-09-13 00:11:17 +0300

    1-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+// class Rectangle that defines a rectangle
+// initializes: w: width, h: height
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+// module.exports exports the class Rectangle
+module.exports = Rectangle;
