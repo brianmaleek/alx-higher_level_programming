01fdd394f663ae0995092c829882316a197b85ec
brianmaleek <maleekmalik@gmail.com>
2023-09-12 21:39:58 +0300

    4-rectangle.js

---
+++ 0x12-javascript-warm_up/0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,39 @@
+#!/usr/bin/node
+
+// class Rectangle that defines a rectangle
+// initializes: w: width, h: height
+// if w or h is equal to 0 or not a positive integer, create an empty object
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      // empty object
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  // prints the rectangle using the character X
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    if (this.width > 0 && this.height > 0) {
+      // empty object
+      [this.width, this.height] = [this.height, this.width];
+    }
+  }
+
+  double () {
+    if (this.width > 0 && this.height > 0) {
+      // empty object
+      this.width = this.width * 2;
+      this.height = this.height * 2;
+    }
+  }
+}
+
+// module.exports exports the class Rectangle
+module.exports = Rectangle;
