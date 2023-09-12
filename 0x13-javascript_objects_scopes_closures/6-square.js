ae5f78cea8fc6abb470803ac713187bcdc21fe29
brianmaleek <maleekmalik@gmail.com>
2023-09-13 00:12:03 +0300

    6-square.js

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+// class square that defines a square and inherits from Rectangle of 4-rectangle.js
+const Rectangle = require('./4-rectangle');
+
+// class Square inherits from Rectangle
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  // method print() prints the rectangle using the character c
+  // if c is undefined, use the character X
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
+    }
+  }
+}
+
+// module.exports exports the class Square
+module.exports = Square;
