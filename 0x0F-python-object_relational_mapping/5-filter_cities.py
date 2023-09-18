7c431de97b9d0a22046f7d7ae1936b3f1ba6d761
brianmaleek <maleekmalik@gmail.com>
2023-09-18 05:06:59 +0300

    5-filter_cities.py

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+"""
+1. Script takes a name of a state as an argument
+    and lists all cities of that state. database used hbtn_0e_4_usa
+2. Takes 4 arguments: mysql username, mysql password,
+database name and state name (SQL injection free!)
+3. Results are sorted in ascending order by states.id
+4. You can use the execute method only once
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+
+    # Open database connection
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+
+    # prepare a cursor object using cursor() method
+    cursor = db.cursor()
+
+    # Define the SQL query to be executed
+    sql_query = "SELECT cities.name FROM cities\
+                JOIN cities ON states.id = cities.state_id\
+                WHERE states.name LIKE %s\
+                ORDER BY cities.id ASC", (argv[4],)
+
+    cursor.execute(sql_query)
+    cities = cursor.fetchall()
+
+    # format the printing of cities of same state separated by commas
+    print(', '.join(["{:s}".format(city[0]) for city in cities]))
+
+    # disconnect from server
+    cursor.close()
+    db.close()
