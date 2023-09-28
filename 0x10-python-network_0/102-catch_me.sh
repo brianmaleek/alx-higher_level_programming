fc2140d494a6389ce3ac76b7b78d890a54a116ff
brianmaleek <maleekmalik@gmail.com>
2023-09-28 23:22:28 +0300

    102-catch_me.sh checker fix 7

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script makes a request to 0.0.0.0:5000/catch_me.
-curl -sL PUT -H /dev/null -o "You got me!" 0.0.0.0:5000/catch_me
+curl -sL -X PUT 0.0.0.0:5000/catch_me -o /dev/null -w "You got me!"
