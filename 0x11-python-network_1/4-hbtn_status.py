ba44efb517a85c0836980c10abf1fab02947665d
brianmaleek <maleekmalik@gmail.com>
2023-09-29 10:57:13 +0300

    4-hbtn_status.py fix 1.0 pycodestyling

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,7 +4,8 @@
 1. Python script fetches https://alx-intranet.hbtn.io/status
 2. Must use the package requests
 3. Only import requests package
-4. Body response must be display like the following example (tabulation before -)
+4. Body response must be display like the following example
+    (tabulation before -)
 """
 
 import requests
