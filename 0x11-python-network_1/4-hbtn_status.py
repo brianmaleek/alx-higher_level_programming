5f1d28ae704a7ba557fe141f109fd2c95370d8a9
brianmaleek <maleekmalik@gmail.com>
2023-09-29 11:00:39 +0300

    4-hbtn_status.py fix 1.1 checker

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -12,14 +12,9 @@ import requests
 
 if __name__ == "__main__":
     url = 'https://intranet.hbtn.io/status'
-
-    try:
-        # send request to URL
-        response = requests.get(url)
-        # display body response
-        print('Body response:')
-        print('\t- type: {}'.format(type(response.text)))
-        print('\t- content: {}'.format(response.text))
-    except requests.exceptions.HTTPError as e:
-        # Handle HTTPError errors
-        print('Error code: {}'.format(e.code))
+    # send request to URL
+    response = requests.get(url)
+    # display body response
+    print('Body response:')
+    print('\t- type: {}'.format(type(response.text)))
+    print('\t- content: {}'.format(response.text))
